aa435c2d8affc69c9eabd4df3002d6d9647b9274
xuxin <917923572@qq.com>
2019-02-28 12:15:58 +0800

    Revert "2.28"
    This reverts commit 78c930c2b29bfbdfbb87976a54129647359e8c78.

--- user.cpp
+++ user.cpp
@@ -1,17 +1,1 @@
 #include"pch.h"
-#include<iostream>
-#include"bank.h"
-
-using namespace std;
-
-void User::setUser(int acc, string nm, int pw, double bal, int sta)
-{
-	account = acc;
-	name = nm;
-	password = pw;
-	balance = bal;
-	state = sta;
-};
-
-
-
