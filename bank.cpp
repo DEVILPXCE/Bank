b587be8c1bb3625d8640e32409f20cc6b4c0af50
DEVILPXCE <43174930+DEVILPXCE@users.noreply.github.com>
2019-02-28 09:13:29 +0800

    添加项目文件。

---
+++ bank.cpp
@@ -0,0 +1,51 @@
+#include"pch.h"
+#include"bank.h"
+#include<iostream>
+#include<cstring>
+#include<fstream>
+
+using namespace std;
+
+void Bank::login()
+{
+	//把文件内数据读入内存后
+	
+	char   stfID[20];
+	char   stfpswd[20];
+	string stfname;
+
+
+	
+	cout <<"请输入您的ID\n";
+	cin >> stfID;
+	//判断在数据库中是否存在该ID号码；
+}
+ Bank::Bank()
+{
+	 ifstream infile;
+	 
+	 infile.open("usersdata", ios::in);
+	 
+	 if (!infile)
+	 {
+		 cout << "OPEN ERROR" << endl;
+		 exit(1);
+	 };
+
+	 int acc;
+	 string nm;
+	 int pw;
+	 int bal;
+	 int sta;
+
+	 int i;
+
+	 while (!infile.eof())
+	 {
+		 //infile >> 
+	 };
+
+
+
+
+}
---
+++ bank.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+const int userN = 10;
+class Bank;
+
+class User 
+{
+public:
+	void setUser(int acc,string nm,int pw,double bal,int sta);
+	void showAccount();
+	bool passwordIsRight();
+	bool UserIsNormal();
+	friend class Bank;
+
+private:
+	int account;
+	string name;
+	int password;
+	int balance;
+	int state;
+
+};
+
+
+
+
+
+class Bank
+{
+public:
+	Bank();//构造函数，涉及文件系统,暂时跳过
+	~Bank();//析构函数，涉及文件系统，暂时跳过
+	void login();//涉及工作人员信息
+	bool loginIsRight();
+	void startwork();
+	void startAccount();
+	void deleteAccount();
+	void save();
+	void draw();
+	void showAccount();
+	void transfer();
+	void reportLoss();
+	void cancleLoss();
+	void changePassword();
+
+private:
+	int N;
+	User users[userN];//！！！改为动态数组
+
+};
