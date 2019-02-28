78c930c2b29bfbdfbb87976a54129647359e8c78
xuxin <917923572@qq.com>
2019-02-28 11:15:28 +0800

    2.28

--- user.cpp
+++ user.cpp
@@ -1,1 +1,17 @@
 #include"pch.h"
+#include<iostream>
+#include"bank.h"
+
+using namespace std;
+
+void User::setUser(int acc, string nm, int pw, double bal, int sta)
+{
+	account = acc;
+	name = nm;
+	password = pw;
+	balance = bal;
+	state = sta;
+};
+
+
+
